e484521ef14af71cc584df71a3de0f350160bad8
Joajak92 <Joakim.jakobsson@iths.segit config --global user.email Joakim.jakobsson@iths.se--global user.name Joajak92--global user.email Joakim.jakobsson@iths.se>
2026-04-08 10:48:04 +0200

    Initial commit

--- src/main/java/se/iths/joakim/webshopprojekt/WebshopProjektApplication.java
+++ src/main/java/se/iths/joakim/webshopprojekt/WebshopProjektApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebshopProjektApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebshopProjektApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopProjektApplication.class, args);
+    }
 
 }
