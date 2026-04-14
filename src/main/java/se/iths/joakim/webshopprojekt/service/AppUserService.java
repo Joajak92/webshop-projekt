c7850c60f1628a8d9d85ab29a37a8266c07d5c57
Joajak92 <Joakim.jakobsson@iths.segit config --global user.email Joakim.jakobsson@iths.se--global user.name Joajak92--global user.email Joakim.jakobsson@iths.se>
2026-04-14 15:19:02 +0200

    skapat AppUserService samt RegisterController

---
+++ src/main/java/se/iths/joakim/webshopprojekt/service/AppUserService.java
@@ -0,0 +1,34 @@
+package se.iths.joakim.webshopprojekt.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import se.iths.joakim.webshopprojekt.model.AppUser;
+import se.iths.joakim.webshopprojekt.repository.AppUserRepository;
+
+@Service
+public class AppUserService {
+
+    private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AppUserService(AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
+        this.appUserRepository = appUserRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public AppUser register(AppUser appUser) {
+        if (appUserRepository.findByUsername(appUser.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("E-postadressen används redan.");
+        }
+        if (!appUser.isConsent()) {
+            throw new IllegalArgumentException();
+        }
+
+        appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
+        if (appUser.getRole() == null || appUser.getRole().isBlank()) {
+            appUser.setRole("USER");
+        }
+
+        return appUserRepository.save(appUser);
+    }
+}
